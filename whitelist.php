9564dd95f5a56668602775bffe74d30e43414cdf
UnstableSolutions <162848579+UnstableSolutions@users.noreply.github.com>
2024-06-14 00:35:48 -0500

    Update whitelist.php

--- whitelist.php
+++ whitelist.php
@@ -1,9 +1,8 @@
 <?php
 $current_data = file_get_contents("keys.json");
 $array_data = json_decode($current_data, true);
-$key = $_GET["key"];
-$encoded_key = hash("sha256", $key);
-if(in_array("$encoded_key", $array_data)){
+$key = $_GET["key"]; // Assuming key is passed through GET parameter, make sure to validate and sanitize it.
+if(in_array($key, $array_data)){
     echo "success";
 }
 else{
